111d04c21e0eb9876a0ceb39d3d67cf343c0d07d
RSurya99 <73375663+RSurya99@users.noreply.github.com>
2020-10-24 11:29:55 +0700

    Update App.py

--- App.py
+++ App.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
-import matplotlib.pyplot as plt
 import covid19_data
 
 app = Flask(__name__)
